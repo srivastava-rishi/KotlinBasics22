e04d7fb4d9ce8cefa8b6b88a0bdd33125489e621
rishisr22 <rishi.srivastava@moneyview.in>
2024-09-29 16:26:12 +0530

    first commit

---
+++ src/main/kotlin/interviewprep/Constructor22.kt
@@ -0,0 +1,17 @@
+package interviewprep
+
+
+fun main() {
+  val cat = Constructor22()
+}
+
+
+class Constructor22 {
+    constructor(){
+        println("Hiii")
+    }
+
+    init {
+        println("lion")
+    }
+}
